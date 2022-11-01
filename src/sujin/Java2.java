b2eaf8e80d9e9a1f0a2237fe6bab4efaf4b4dae5
sujin9335 <sujin93359335@gmail.com>
2022-11-02 00:53:02 +0900

    11.2

--- src/sujin/Java2.java
+++ src/sujin/Java2.java
@@ -27,20 +27,20 @@ public class Java2 {
 //		}
 			
 		
-		System.out.println("switch()");
-		switch(7) {
-		case 1:
-			System.out.println("1");
-		case 2:
-			System.out.println("2");
-		case 3:
-			System.out.println("3");
-			break;
-		case 4:
-			System.out.println("4");
+//		System.out.println("switch()");
+//		switch(7) {
+//		case 1:
+//			System.out.println("1");
+//		case 2:
+//			System.out.println("2");
+//		case 3:
+//			System.out.println("3");
+//			break;
+//		case 4:
+//			System.out.println("4");
 //		default:
 //			System.out.println("default");
-		}
+//		}
 		
 		
 //		int a = 2;
