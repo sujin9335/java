26c9f56eb1b6be85afebfce73480da03af85eae7
sujin9335 <sujin93359335@gmail.com>
2022-11-02 00:52:30 +0900

    11.2

--- src/sujin/Java1.java
+++ src/sujin/Java1.java
@@ -4,7 +4,36 @@ public class Java1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello word");
+//		int a; // 기본 int
+//		a = 1;
+//		System.out.println(a+1);
+//		
+//		double a; // 소숫점 double
+//		b = 1.1;
+//		System.out.println(b);
+//		
+//		String a = "coding"; // 문자 String
+//		System.out.println(c);
+		
+		
+//		int a = 10;
+//		a = a%7; // 나머지 % 
+//		System.out.println(a);
+//		a = a/3; // 몫 / 
+//		System.out.println(a);
+//		
+//		a++; // a에 + 1
+//		++a; // a에 + 1
+//		System.out.println(a);
+	
+//		System.out.println(1==2); // 비교 == ture false 로 출력 
+//		System.out.println(1!=2); // 비교 != ture false 반대로 출력
+//		String a = "Hello";
+//		String b = new String("Hello");
+//		System.out.println(a==b); // 문자열 비교
+//		System.out.println(a.equals(b)); // 문자열 비교 할때 .equals()를 사용한다
+		
+		
 		
 		
 	}
---
+++ src/sujin/Java2.java
@@ -0,0 +1,60 @@
+package sujin;
+
+public class Java2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		if(false) {
+//			System.out.println(1);
+//		} else if(true) {
+//			System.out.println(2);
+//		} else {
+//			System.out.println(3);
+//		}
+		
+//		String id = "sujin";
+//		String password = "kim";
+//		if(id.equals("sujin")) {
+//			if(password.equals("kim")) {
+//				System.out.println("ok");
+//			} else {
+//				System.out.println("no");
+//				}
+//			 
+//		} else  {
+//			System.out.println("no");
+//		}
+			
+		
+		System.out.println("switch()");
+		switch(7) {
+		case 1:
+			System.out.println("1");
+		case 2:
+			System.out.println("2");
+		case 3:
+			System.out.println("3");
+			break;
+		case 4:
+			System.out.println("4");
+//		default:
+//			System.out.println("default");
+		}
+		
+		
+//		int a = 2;
+//		if(a == 1) {
+//			System.out.println("1");
+//		}else if(a == 2) {
+//			System.out.println("2");
+//		}else if(a == 3) {
+//			System.out.println("3");
+//		}
+		
+		
+		
+		
+	}
+
+}
